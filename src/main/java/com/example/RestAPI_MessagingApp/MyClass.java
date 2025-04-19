254961deb69a66f4e80fadaf025be0d3df10f2c5
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-04-19 14:51:25 +0530

    Implemented UC4 | Check Entity_mapping.java

--- src/main/java/com/example/RestAPI_MessagingApp/MyClass.java
+++ src/main/java/com/example/RestAPI_MessagingApp/MyClass.java
@@ -1,9 +1,6 @@
 package com.example.RestAPI_MessagingApp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 class MyClass{
@@ -21,4 +18,5 @@ class MyClass{
     public String sayName(@PathVariable String name){
         return "Hello "+name ;
     }
+
 }
